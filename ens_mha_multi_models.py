6bf052c34833c3fe5b12134f128cdad0d9c66788
James Sheldon <129717481+Jamesinza@users.noreply.github.com>
2026-01-30 12:46:42 +0200

    Update ens_mha_multi_models.py

--- ens_mha_multi_models.py
+++ ens_mha_multi_models.py
@@ -65,14 +65,16 @@ def make_lstm_model(input_shape, dropout, dims, num_layers):
     x = norm(inputs)
     # pos_enc = SinePositionEncoding()(x)
     # x = layers.Add(name='model_add')([x, pos_enc])
-    x = layers.LSTM(dims, return_sequences=True, name='model_lstm')(x)
+    x = layers.Dense(dims, name='model_dense')(x)
+
     for i in range(num_layers):
         x = lstm_basic(x, dims, dropout, i)
+
     x = layers.Flatten(name='lmodel_flat')(x)
-    # flat_dim = x.shape[1]
-    # for dim in [128]:
-    #     x = layers.Dense(dim, activation='relu')(x)
-    #     x = layers.Dropout(dropout)(x)
+    flat_dim = x.shape[1]
+    for dim in [flat_dim//2]:
+        x = layers.Dense(dim, activation='relu')(x)
+        x = layers.Dropout(dropout)(x)
     outputs = layers.Dense(10)(x)
     return models.Model(inputs, outputs)    
     
@@ -182,9 +184,6 @@ def rolling_count_fixed_window(data, column_name):
         value = data[column_name][i]
         # Add current index to the tracking list for the current value
         value_positions[value].append(i)
-        # Remove indices outside the fixed window
-        # while value_positions[value] and value_positions[value][0] < i - window_size + 1:
-        #     value_positions[value].pop(0)
         # Count the number of occurrences within the fixed window
         rolling_counts[i] = len(value_positions[value])
     # Add the feature to the DataFrame
@@ -197,7 +196,6 @@ def distance_to_next(data, column_name):
     Calculate the distance (in rows) to the next occurrence of the same value.
     """
     data = data.reset_index(drop=True)
-    # print(f'\nCalculating the distance (in rows) to the next occurrence of the same value....')
     next_seen = {}
     distance = [0] * len(data)
     for i in range(len(data) - 1, -1, -1):
@@ -223,31 +221,26 @@ def create_windows(data, wl):
 def analyze_misclassifications(model, X, y_true, class_names=None):
     """
     Analyze misclassified samples for a classification model.
-
     Parameters:
     - model: Trained classification model.
     - X: Input features (NumPy array or DataFrame).
     - y_true: True labels (array-like).
     - class_names: List of class names for better interpretability (optional).
-
     Returns:
     - DataFrame summarizing misclassified samples.
     """
-    
     # Predict and find misclassified samples
     y_pred = np.argmax(model.predict(X), axis=1)
     misclassified_indices = np.where(y_pred != y_true)[0]
     misclassified_X = X[misclassified_indices]
     misclassified_y_true = y_true[misclassified_indices]
     misclassified_y_pred = y_pred[misclassified_indices]
-
     # Summarize misclassifications
     misclassified_df = pd.DataFrame({
         'True Label': misclassified_y_true,
         'Predicted Label': misclassified_y_pred,
         'Sample Index': misclassified_indices
     })
-
     # Add class names if provided
     if class_names:
         misclassified_df['True Label'] = misclassified_df['True Label'].map(lambda x: class_names[x])
@@ -258,7 +251,6 @@ def analyze_misclassifications(model, X, y_true, class_names=None):
 def evaluate_test_set(model, X_test, y_test, class_names=None):
     """
     Evaluate the model's performance on the test set and visualize results.
-
     Parameters:
     - model: Trained classification model.
     - X_test: Test input features (NumPy array or DataFrame).
@@ -267,11 +259,9 @@ def evaluate_test_set(model, X_test, y_test, class_names=None):
     """
     # Predict class probabilities and convert to predicted labels
     y_pred = np.argmax(model.predict(X_test), axis=1)
-
     # Classification report
     print("Classification Report:")
     print(classification_report(y_test, y_pred, target_names=class_names))
-
     # # Confusion matrix
     # cm = confusion_matrix(y_test, y_pred)
     # plt.figure(figsize=(8, 6))
@@ -291,12 +281,12 @@ if __name__ == '__main__':
     num_layers = 2
     activation = 'relu'
     
-    batch_size = 16
+    batch_size = 32
     lr = 5e-4
     early_pat = 5
     reduc_pat = 2
     reduc_fac = 0.9
-
+    
     # Target dataset
     target_df = pd.read_csv(f'datasets/{lotto}_Full.csv')
     target_df = (target_df[['A','B','C','D','E']].dropna().reset_index(drop=True).astype(np.int8))
@@ -319,7 +309,7 @@ if __name__ == '__main__':
     # Flatten down to individual digits
     print('\nFlattening down to individual digits')
     quick_df = np.array([int(digit) for number in quick_df for digit in str(number)], dtype=np.int8)
-    quick_df = quick_df[int(len(quick_df)*0.95):int(len(quick_df)*0.953)]
+    quick_df = quick_df[int(len(quick_df)*0.95):]
 
     full_data = quick_df  #np.concatenate((quick_df, target_df))
     len_target = len(target_df)
@@ -329,94 +319,94 @@ if __name__ == '__main__':
     # Creating new df from the digit data with targets
     full_data = pd.DataFrame(full_data, columns=['0'])
 
-    # Shift target positions per iteration
-    for t in range(1):
-        data = full_data.copy()
-        print(f'\nNow training for T{t} target...')
-        data['targets'] = data['0'].shift(-(t+1))
-        data.dropna(inplace=True)
-        data['targets'] = data['targets'].astype(np.int8)
-        
-        # Creating new features
-        print('\nCreating new features')
-        col = '0' # Column to use for feature calculations
-        
-        # Splitting X and y values
-        target = data['targets'].values
-        data = data.drop(columns=['targets'])
-        
-        print(f'\nTarget Data Length: {len_target}\tQuick Data Length: {len_quick}')
-        
-        windows = create_windows(data, wl)
-        
-        enc_in = windows[1:]
-        dec_out = target[wl:-1]
+    t = 0
+    data = full_data.copy()
+    print(f'\nNow training for T{t} target...')
+    data['targets'] = data['0'].shift(-(t+1))
+    data.dropna(inplace=True)
+    data['targets'] = data['targets'].astype(np.int8)
+    
+    # Creating new features
+    print('\nCreating new features')
+    col = '0' # Column to use for feature calculations
+    
+    # Splitting X and y values
+    target = data['targets'].values
+    unique_classes = np.unique(target)
+    data = data.drop(columns=['targets'])
+    
+    print(f'\nTarget Data Length: {len_target}\tQuick Data Length: {len_quick}')
+    
+    windows = create_windows(data, wl)
+    
+    enc_in = windows[1:]
+    dec_out = target[wl:-1]
 
-        # print(f'\nenc_in:\n{enc_in[-3:]}')
-        # print(f'\ndec_out:\n{dec_out[-3:]}\n')
-        
-        split = len(enc_in)//10
-        encoder_input = enc_in[:split*8]  #enc_in[:int(len_quick+(len_target*0.8))]
-        decoder_output = dec_out[:split*8]  #dec_out[:int(len_quick+(len_target*0.8))]
-        norm = layers.Normalization(name='data_norm')
-        norm.adapt(encoder_input)
-        
-        enc_in_test = enc_in[split*8:split*9]  #enc_in[int(len_quick+(len_target*0.8)):int(len_quick+(len_target*0.95))]
-        dec_out_test = dec_out[split*8:split*9]  #dec_out[int(len_quick+(len_target*0.8)):int(len_quick+(len_target*0.95))]
-        
-        enc_in_val = enc_in[split*9:]  #enc_in[-int(len_target*0.05):]
-        dec_out_val = dec_out[split*9:]  #dec_out[-int(len_target*0.05):]
-        
-        input_shape = (encoder_input.shape[1],encoder_input.shape[2])
-        
-        model = make_lstm_model(input_shape, dropout, dims, num_layers)
-        model_name = f'lstm_mha_t{t}_WL{wl}_digits_lin_v2'
-        model_weights = f'model_weights/{model_name}.weights.h5'
+    # print(f'\nenc_in:\n{enc_in[-3:]}')
+    # print(f'\ndec_out:\n{dec_out[-3:]}\n')
+    
+    split = len(enc_in)//10
+    encoder_input = enc_in[:split*8]  #enc_in[:int(len_quick+(len_target*0.8))]
+    decoder_output = dec_out[:split*8]  #dec_out[:int(len_quick+(len_target*0.8))]
+    norm = layers.Normalization(name='data_norm')
+    norm.adapt(encoder_input)
+    
+    enc_in_test = enc_in[split*8:split*9]  #enc_in[int(len_quick+(len_target*0.8)):int(len_quick+(len_target*0.95))]
+    dec_out_test = dec_out[split*8:split*9]  #dec_out[int(len_quick+(len_target*0.8)):int(len_quick+(len_target*0.95))]
+    
+    enc_in_val = enc_in[split*9:]  #enc_in[-int(len_target*0.05):]
+    dec_out_val = dec_out[split*9:]  #dec_out[-int(len_target*0.05):]
+    
+    input_shape = (encoder_input.shape[1],encoder_input.shape[2])
+    
+    model = make_lstm_model(input_shape, dropout, dims, num_layers)
+    model_name = f'combo_mha_t{t}_WL{wl}_digits_class_v4'
+    model_weights = f'model_weights/{model_name}.weights.h5'
 
-        # model.load_weights(model_weights)
-        
-        model.compile(optimizer=optimizers.AdamW(learning_rate=lr), loss=losses.SparseCategoricalCrossentropy(use_logits=True), metrics=['accuracy'], jit_compile=True)
-        model.summary()
-        
-        callback = [callbacks.EarlyStopping(monitor='val_mae', min_delta=0,
-                                            patience=early_pat, verbose=1, mode='auto',
-                                            baseline=None, restore_best_weights=True,
-                                            start_from_epoch=0),
-                   callbacks.ReduceLROnPlateau(monitor='val_loss', factor=reduc_fac,
-                                               patience=reduc_pat, verbose=0, mode='auto',
-                                               min_delta=0.0, cooldown=0, min_lr=0),
-                    callbacks.ModelCheckpoint(f'checkpoint_models/{model_name}.keras',
-                                              monitor = 'val_mae', save_best_only=True)
-                   ]
-        
-        # Training the model
-        #class_weights = compute_class_weight('balanced', classes=unique_classes, y=decoder_output)
-        #class_weights_dict = dict(enumerate(class_weights))
-        
-        lstm_history = model.fit(encoder_input, decoder_output, batch_size=batch_size, epochs=100, verbose=1,
-                           validation_data=(enc_in_test, dec_out_test), callbacks=callback,) # class_weight=class_weights_dict)
-        
-        model.save(f'models/{model_name}.keras', overwrite=True)
-        model.save_weights(model_weights, overwrite=True)
-        
-        val_loss, val_mae, _ = model.evaluate(enc_in_val, dec_out_val)
-        print(f'\nVal Loss: {val_loss:.4f}\tVal MAE: {val_mae:.4f}\n')
-        
-        # nums = 500
-        #predictions = model.predict(enc_in_val)
-        #preds = np.argmax(predictions, axis=1)
-        #real_res = dec_out_val
-        #hits = 0
-        #for i, num in enumerate(preds):
-        #    if num==real_res[i]:
-        #        hits += 1
-        #print(f'\nCorrect Predictions = {hits} out of {len(real_res)}')
-        #print(f'\nenc_in_val:\n{enc_in_val[-3:]}')
-        #print(f'\ndec_out_val:\n{dec_out_val[-3:]}\n')
-        
-        #evaluate_test_set(model, enc_in_val, dec_out_val, class_names=['Class 0', 'Class 1', 'Class 2','Class 3', 'Class 4', 'Class 5','Class 6', 'Class 7', 'Class 8','Class 9'])
-        
-        #misclassified_df = analyze_misclassifications(model, enc_in_val, dec_out_val, class_names=['Class 0', 'Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6', 'Class 7', 'Class 8', 'Class 9'])
-        #print(f'\nMisClassified Classes:\n{misclassified_df.head()}')
-        del windows, model
-        gc.collect()
+    # model.load_weights(model_weights)
+    
+    model.compile(optimizer=optimizers.AdamW(learning_rate=lr), loss=losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'], jit_compile=True)
+    model.summary()
+    
+    callback = [callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0,
+                                        patience=early_pat, verbose=1, mode='auto',
+                                        baseline=None, restore_best_weights=True,
+                                        start_from_epoch=0),
+               callbacks.ReduceLROnPlateau(monitor='val_loss', factor=reduc_fac,
+                                           patience=reduc_pat, verbose=0, mode='auto',
+                                           min_delta=0.0, cooldown=0, min_lr=0),
+                callbacks.ModelCheckpoint(f'checkpoint_models/{model_name}.keras',
+                                          monitor = 'val_accuracy', save_best_only=True)
+               ]
+    
+    # Training the model
+    class_weights = compute_class_weight('balanced', classes=unique_classes, y=decoder_output)
+    class_weights_dict = dict(enumerate(class_weights))
+    
+    lstm_history = model.fit(encoder_input, decoder_output, batch_size=batch_size, epochs=100, verbose=1,
+                       validation_data=(enc_in_test, dec_out_test), callbacks=callback, class_weight=class_weights_dict)
+    
+    model.save(f'models/{model_name}.keras', overwrite=True)
+    model.save_weights(model_weights, overwrite=True)
+    
+    val_loss, val_mae = model.evaluate(enc_in_val, dec_out_val)
+    print(f'\nVal Loss: {val_loss:.4f}\tVal ACCURACY: {val_mae:.4f}\n')
+    
+    nums = 100
+    predictions = model.predict(enc_in_val)
+    preds = np.argmax(predictions, axis=1)
+    real_res = dec_out_val
+    hits = 0
+    for i, num in enumerate(preds):
+       if num==real_res[i]:
+           hits += 1
+    print(f'\nCorrect Predictions = {hits} out of {len(real_res)}')
+    print(f'\nenc_in_val:\n{enc_in_val[-3:]}')
+    print(f'\ndec_out_val:\n{dec_out_val[-3:]}\n')
+    
+    evaluate_test_set(model, enc_in_val, dec_out_val, class_names=['Class 0', 'Class 1', 'Class 2','Class 3', 'Class 4', 'Class 5','Class 6', 'Class 7', 'Class 8','Class 9'])
+    
+    misclassified_df = analyze_misclassifications(model, enc_in_val, dec_out_val, class_names=['Class 0', 'Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6', 'Class 7', 'Class 8', 'Class 9'])
+    print(f'\nMisClassified Classes:\n{misclassified_df.head()}')
+    del windows, model
+    gc.collect()
